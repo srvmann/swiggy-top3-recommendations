a16e2559ba8cd9fe6b5080c04fc1e3e057e5b3f6
srvmann <sauravgrover567@gmail.com>
2025-07-09 19:35:15 +0530

    Cookie-cutter-initial-commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This will return the top 3 recommendation from swiggy restaurant dataset',
+    author='Saurav',
+    license='',
+)
